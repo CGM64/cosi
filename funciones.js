e4bb2bc42ac8c5a3861c98a86b56866afb8c750a
Charly <charly@example.com>
2025-01-18 08:15:43 -0600

    [REF] Subiendo primeros cambios

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function mostrarMensaje() {
+    // const mensaje = "Hola mi amor, si llegasta hasta aquí significa que te quedarás conmigo por siempre wajajaja🤗😍. Presiona enviar para continuar😘.";
+    // const numeroWhatsApp = "+50248439450";
+    // const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
+    // window.open(url, "_blank");
+
+    alert('Mi amor, te amo con todo mi corazón❤️');
+    alert('Eres la mujer que estaba esperando y con la que quiero estar el resto de mi vida😍🥰❤️');
+
+    // Redirigir a otro archivo HTML en el proyecto
+    window.location.href = "fotitos.html";  
+}
