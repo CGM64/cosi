3e98476b8a744df56b565fe47bfde160faedfa9d
Charly <charly@example.com>
2025-01-19 20:25:12 -0600

    [REF] Agregando api para WP

--- funciones.js
+++ funciones.js
@@ -1,9 +1,4 @@
 function mostrarMensaje() {
-    // const mensaje = "Hola mi amor, si llegasta hasta aquí significa que te quedarás conmigo por siempre wajajaja🤗😍. Presiona enviar para continuar😘.";
-    // const numeroWhatsApp = "+50248439450";
-    // const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
-    // window.open(url, "_blank");
-
     alert('Mi amor, te amo con todo mi corazón❤️');
     alert('Eres la mujer que estaba esperando y con la que quiero estar el resto de mi vida😍🥰❤️');
 
